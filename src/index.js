8b9f10b310723db47130ed3d530979b628d3e11d
Armando Martinez <alemar.martinez16@gmail.com>
2023-09-20 20:04:33 -0300

    se modulariza elarchivo index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import express from "express";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
-//importar routes
-import indexRoutes from "./routes/index.js";
+import bodyParser from "body-parser";
+
+import contactRouter from "./routes/contactRouter.js"; // Importa el router de contacto
 
 const app = express();
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
-//console.log(join(__dirname, "views"));
-
 app.set("views", join(__dirname, "views"));
 app.set("view engine", "ejs");
-
-app.use(indexRoutes);
 app.use(express.static(join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000);
-console.log("Servidor corriendo en el puerto 3000");
+// Utiliza el router de contacto para la ruta "/contactRouter"
+app.use("/contact", contactRouter);
+
+// Define otras rutas aquí utilizando routers similares
+
+app.get("/", (req, res) => res.render("index", { title: "Home" }));
+app.get("/about", (req, res) => res.render("about", { title: "About" }));
+app.get("/contact", (req, res) => res.render("contact", { title: "Contact" }));
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
