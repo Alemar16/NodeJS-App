cd6f481668f7a7fac0bf85a22082cd170cc48c71
Armando Martinez <alemar.martinez16@gmail.com>
2023-09-22 11:10:18 -0300

    PORT en variable de entorno

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import bodyParser from "body-parser";
 import contactRouter from "./routes/contactRouter.js"; // Importa el router de contacto
 import { config } from "dotenv"; // Importa el módulo dotenv
 
-config(); // Carga las variables de entorno desde el archivo .env
+config();// Carga las variables de entorno desde el archivo .env
+
+const PORT = process.env.PORT_SERVER_NODE;
 
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -36,34 +38,11 @@ app.get("/contact", (req, res) =>
   })
 );
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+app.listen(process.env.PORT || process.env.PORT_SERVER_NODE)
+  console.log(
+    `Server is running on port ${
+      process.env.PORT || process.env.PORT_SERVER_NODE
+    }`
+  );
 
-// import express from "express";
-// import { dirname, join } from "path";
-// import { fileURLToPath } from "url";
-// import bodyParser from "body-parser";
 
-// import contactRouter from "./routes/contactRouter.js"; // Importa el router de contacto
-
-// const app = express();
-// const __dirname = dirname(fileURLToPath(import.meta.url));
-// app.set("views", join(__dirname, "views"));
-// app.set("view engine", "ejs");
-// app.use(express.static(join(__dirname, "public")));
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// // Utiliza el router de contacto para la ruta "/contactRouter"
-// app.use("/contact", contactRouter);
-
-// // Define otras rutas aquí utilizando routers similares
-
-// app.get("/", (req, res) => res.render("index", { title: "Home" }));
-// app.get("/about", (req, res) => res.render("about", { title: "About" }));
-// //app.get("/contact", (req, res) => res.render("contact", { title: "Contact" }));
-// app.get("/contact", (req, res) => res.render("contact", { title: "Contact", recipientEmail, senderEmail }));
-
-// app.listen(3000, () => {
-//   console.log("Server is running on port 3000");
-// });
