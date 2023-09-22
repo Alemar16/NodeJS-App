6ca122e4ea391c803ea4a3b1a8658cec4fe3a322
Armando Martinez <alemar.martinez16@gmail.com>
2023-09-22 03:23:29 -0300

    se renombreo titulos en pgas

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => res.render("index", { title: "My First page with NodeJS" }));
-router.get("/about", (req, res) => res.render("about", { title: "About" }));
-router.get("/contact", (req, res) => res.render("contact", { title: "Contact" }));
+router.get("/", (req, res) => res.render("index", { title: "NodeJS Project" }));
+router.get("/about", (req, res) => res.render("about", { title: "NodeJS Project" }));
+router.get("/contact", (req, res) => res.render("contact", { title: "NodeJS Project" }));
 
 export default router;
